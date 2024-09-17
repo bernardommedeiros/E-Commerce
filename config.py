db72cf34ff73cd61086ae30895dcf9ad0d08da35
Nicollas Matheus Prado Pinheiro <20241014040014@ifrn.local>
2024-09-17 11:21:57 -0300

    configs

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import pymysql
 
 DEBUG = True
 
-MYSQL_CONN = pymysql.connect(host='nicollasprado.mysql.pythonanywhere-services.com', port=3306, user='root', password='nicollas14', database='ecommerce')
+MYSQL_CONN = pymysql.connect(host='nicollasprado.mysql.pythonanywhere-services.com', port=3306, user='root', password='ecommerce14', database='ecommerce')
