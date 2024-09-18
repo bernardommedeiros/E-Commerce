1265a27956fac3f0d24b01cee55c6e516f393898
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-09-18 16:06:42 -0300

    adição da tela de registro e pagina de acessorios

--- ecommerce/controllers/main.py
+++ ecommerce/controllers/main.py
@@ -15,6 +15,10 @@ def feminino():
 def feminino_tenis():
     return render_template('tenisFeminino.html')
 
+@app.route("/feminino_acessorios")
+def feminino_acessorios():
+    return render_template('acessoriosFeminino.html')
+
 @app.route("/masculino_vestuario")
 def masculino():
     return render_template('masculino.html')
@@ -22,3 +26,7 @@ def masculino():
 @app.route("/masculino_tenis")
 def masculino_tenis():
     return render_template('tenisMasculino.html')
+
+@app.route("/masculino_acessorios")
+def masculino_acessorios():
+    return render_template('acessoriosMasculino.html')
